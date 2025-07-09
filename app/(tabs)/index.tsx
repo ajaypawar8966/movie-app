be2548d99baf21952592fe01c2f9389ff59b6de1
Ajay <ajaypawar8966@gmail.com>
2025-07-09 13:11:27 +0530

    hide error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -32,7 +32,7 @@ export default function Index() {
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView className="flex-1 px-5">
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading ? (
+        {/* {moviesLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
@@ -43,33 +43,33 @@ export default function Index() {
             {" "}
             Error:{moviesError?.message}
           </Text>
-        ) : (
-          <View className="flex-1 mt-5">
-            <SearchBar
-              onPress={() => router.push("/search")}
-              placeholder="Search for a movie"
+        ) : ( */}
+        <View className="flex-1 mt-5">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
+          <>
+            <Text className="text-lg text-white  font-bold mt-5 mb-3">
+              Latest Movies
+            </Text>
+            <FlatList
+              data={staticMovies}
+              renderItem={({ item }) => <MovieCard {...item} />}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10,
+              }}
+              scrollEnabled={false}
+              className="mt-2 pb-32"
             />
-            <>
-              <Text className="text-lg text-white  font-bold mt-5 mb-3">
-                Latest Movies
-              </Text>
-              <FlatList
-                data={staticMovies}
-                renderItem={({ item }) => <MovieCard {...item} />}
-                keyExtractor={(item) => item.id.toString()}
-                numColumns={3}
-                columnWrapperStyle={{
-                  justifyContent: "flex-start",
-                  gap: 20,
-                  paddingRight: 5,
-                  marginBottom: 10,
-                }}
-                scrollEnabled={false}
-                className="mt-2 pb-32"
-              />
-            </>
-          </View>
-        )}
+          </>
+        </View>
+        {/* )} */}
       </ScrollView>
     </View>
   );
